511f6a53c860b05b67321854068efed806a647b1
WingHaa <quanlm24@gmail.com>
2023-11-06 07:41:40 +0700

    Fix noice config

--- lua/plugins/noice-nvim.lua
+++ lua/plugins/noice-nvim.lua
@@ -1,18 +1,6 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
-	opts = {
-		messages = {
-			-- NOTE: If you enable messages, then the cmdline is enabled automatically.
-			-- This is a current Neovim limitation.
-			enabled = true,         -- enables the Noice messages UI
-			view = "notify",        -- default view for messages
-			view_error = "notify",  -- view for errors
-			view_warn = "notify",   -- view for warnings
-			view_history = "messages", -- view for :messages
-			view_search = false,    -- view for search count messages. Set to `false` to disable
-		},
-	},
 	dependencies = {
 		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
@@ -34,6 +22,16 @@ return {
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false,       -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false,   -- add a border to hover docs and signature help
+			},
+			messages = {
+				-- NOTE: If you enable messages, then the cmdline is enabled automatically.
+				-- This is a current Neovim limitation.
+				enabled = true,        -- enables the Noice messages UI
+				view = "notify",       -- default view for messages
+				view_error = "notify", -- view for errors
+				view_warn = "notify",  -- view for warnings
+				view_history = "messages", -- view for :messages
+				view_search = false,   -- view for search count messages. Set to `false` to disable
 			},
 		})
 	end
