3bf94e1c581c1a2b57bff1e818310fec0c8a5b22
lmquan.sgt <lmquan.sgt@gmail.com>
2024-01-08 10:36:48 +0700

    Fix Comment not loading when using in Visual mode

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 require("config")
--- vim.cmd("colorscheme tokyonight-moon")
+vim.cmd("colorscheme tokyonight-moon")
 -- vim.cmd("colorscheme onedark")
 -- vim.cmd("colorscheme catppuccin")
-vim.cmd("colorscheme cyberdream") -- set the colorscheme
--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,6 @@
 return {
 	"numToStr/Comment.nvim",
+	event = "BufReadPost",
 	keys = { "gc", "gb" },
 	config = function()
 		local pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
