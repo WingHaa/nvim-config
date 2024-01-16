0398e8def37ac79cf5e64651c67b2ceff7fc37fc
WingHaa <quanlm24@gmail.com>
2024-01-16 07:18:06 +0700

    use fzflua for code action

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -7,7 +7,33 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		-- calling `setup` is optional for customization
-		require("fzf-lua").setup({ "telescope" })
+		require("fzf-lua").setup({
+			"telescope",
+			winopts = { preview = { default = "bat" } },
+			previewers = {
+				bat = {
+					cmd = "bat",
+					args = "--color=always --style=numbers,changes",
+					-- uncomment to set a bat theme, `bat --list-themes`
+					theme = "OneHalfDark",
+				},
+			},
+			files = {
+				prompt = "Files❯ ",
+				find_opts = [[-type f -not -path '*/\.git/*' -printf '%P\n']],
+				rg_opts = "--color=never --files --hidden --follow -g '!.git'",
+				fd_opts = "--color=never --type f --hidden --follow --exclude .git",
+				actions = {
+					-- inherits from 'actions.files', here we can override
+					-- or set bind to 'false' to disable a default action
+					--   ["default"]   = actions.file_edit,
+					-- custom actions are available too
+					--   ["ctrl-y"]    = function(selected) print(selected[1]) end,
+					-- action to toggle `--no-ignore`, requires fd or rg installed
+					--   ["ctrl-g"]    = { actions.toggle_ignore },
+				},
+			},
+		})
 
 		map.set("n", "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>", desc("Files by name"))
 		map.set("n", "<leader>fg", "<cmd>lua require('fzf-lua').grep()<CR>", desc("Grep pattern"))
@@ -19,7 +45,7 @@ return {
 		map.set("n", "<leader>fW", "<cmd>lua require('fzf-lua').grep_cWORD()<CR>", desc("WORD at cursor"))
 		map.set("n", "<leader>fr", "<cmd>lua require('fzf-lua').resume()<CR>", desc("Resume"))
 
-		map.set("n", "<leader>fc", "<cmd>lua require('fzf-lua').lsp_code_actions()<CR>", desc("Code Actions"))
+		map.set({ "n", "v" }, "<leader>ca", "<cmd>lua require('fzf-lua').lsp_code_actions()<CR>", desc("Code Actions"))
 		map.set("n", "<leader>fo", "<cmd>lua require('fzf-lua').lsp_references()<CR>", desc("References"))
 		map.set("n", "<leader>fd", "<cmd>lua require('fzf-lua').lsp_definitions()<CR>", desc("Definitions"))
 		map.set("n", "<leader>ft", "<cmd>lua require('fzf-lua').lsp_typedefs()<CR>", desc("Type definition"))
