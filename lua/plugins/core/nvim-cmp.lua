69c9a1356e11bd8b8b6fc9cebff46df75d2c3365
WingHaa <quanlm24@gmail.com>
2024-11-09 19:48:38 +0700

    switch to blink.cmp from nvim.cmp
    switch to blink.cmp from nvim.cmp
    
    update blink

--- lua/plugins/core/nvim-cmp.lua
+++ lua/plugins/core/nvim-cmp.lua
@@ -1,118 +1,130 @@
 local M = {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
+    "saghen/blink.cmp",
+    version = "v0.*",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts_extend = { "sources.completion.enabled_providers" },
 }
-
-M.opts = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    local lspkind = require("lspkind")
-    local neogen = require("neogen")
-
-    return {
-        experimental = {
-            ghost_text = true,
-        },
-        snippet = {
-            expand = function(args)
-                luasnip.lsp_expand(args.body)
-            end,
-        },
-        mapping = cmp.mapping.preset.insert({
-            ["<C-p>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-            ["<C-n>"] = cmp.mapping.select_next_item(), -- next suggestion
-            ["<C-b>"] = cmp.mapping.scroll_docs(-4), -- back
-            ["<C-f>"] = cmp.mapping.scroll_docs(4), -- forward
-            ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-            ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-            ["<CR>"] = cmp.mapping.confirm({ select = false }),
-            ["<tab>"] = cmp.mapping(function(fallback)
-                if neogen.jumpable() then
-                    neogen.jump_next()
-                else
-                    fallback()
-                end
-            end, {
-                "i",
-                "s",
-            }),
-            ["<S-tab>"] = cmp.mapping(function(fallback)
-                if neogen.jumpable(true) then
-                    neogen.jump_prev()
-                else
-                    fallback()
-                end
-            end, {
-                "i",
-                "s",
-            }),
-        }),
-        -- sources for autocompletion
-        sources = cmp.config.sources({
-            { name = "nvim_lsp" }, -- lsp
-            { name = "luasnip" }, -- snippets
-            { name = "buffer" }, -- text within current buffer
-            { name = "path" }, -- file system paths
-            { name = "codeium" },
-        }),
-        -- configure lspkind for vs-code like icons
-        formatting = {
-            format = lspkind.cmp_format({
-                maxwidth = 80,
-                ellipsis_char = "...",
-                symbol_map = {
-                    Text = "󰉿",
-                    Method = "󰆧",
-                    Function = "󰊕",
-                    Constructor = "",
-                    Field = "󰜢",
-                    Variable = "󰀫",
-                    Class = "󰠱",
-                    Interface = "",
-                    Module = "",
-                    Property = "󰜢",
-                    Unit = "󰑭",
-                    Value = "󰎠",
-                    Enum = "",
-                    Keyword = "󰌋",
-                    Snippet = "",
-                    Color = "󰏘",
-                    File = "󰈙",
-                    Reference = "󰈇",
-                    Folder = "󰉋",
-                    EnumMember = "",
-                    Constant = "󰏿",
-                    Struct = "󰙅",
-                    Event = "",
-                    Operator = "󰆕",
-                    TypeParameter = "",
-                    Codeium = "",
-                },
-            }),
-        },
-    }
-end
-
-M.config = function(_, opts)
-    require("luasnip/loaders/from_vscode").lazy_load()
-    require("cmp").setup(opts)
-    vim.opt.completeopt = "menu,menuone,noselect"
-end
 
 M.dependencies = {
-    "onsails/lspkind.nvim",
     {
-        "L3MON4D3/LuaSnip",
-        -- follow latest release.
-        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-        -- install jsregexp (optional!).
-        build = "make install_jsregexp",
+        "saghen/blink.compat",
+        opts = {
+            impersonate_nvim_cmp = false,
+            enable_events = false,
+            debug = false,
+        },
     },
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-nvim-lsp",
+    {
+        "Exafunction/codeium.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        cmd = "Codeium",
+        build = ":Codeium Auth",
+        opts = {},
+    },
 }
+
+M.opts = {
+    keymap = { preset = "enter" },
+    completion = {
+        trigger = {
+            list = {
+                max_items = 200,
+                selection = "manual",
+            },
+
+            accept = {
+                auto_brackets = {
+                    enabled = true,
+                },
+            },
+
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 200,
+            },
+
+            ghost_text = {
+                enabled = true,
+            },
+        },
+        menu = {
+            draw = {
+                columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
+            },
+        },
+    },
+
+    signature = {
+        enabled = true,
+    },
+
+    sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+        cmdline = {},
+
+        min_keyword_length = function()
+            return vim.bo.filetype == "markdown" and 2 or 0
+        end,
+
+        completion = {
+            enabled_providers = {
+                "lsp",
+                "path",
+                "snippets",
+                "buffer",
+                "luasnip",
+                "dadbod",
+                "codeium",
+                "lazydev",
+            },
+        },
+
+        providers = {
+            lsp = {
+                name = "LSP",
+                module = "blink.cmp.sources.lsp",
+                transform_items = function(_, items)
+                    return vim.tbl_filter(function(item)
+                        return item.kind ~= require("blink.cmp.types").CompletionItemKind.Text
+                    end, items)
+                end,
+            },
+            snippets = {
+                name = "Snippets",
+                module = "blink.cmp.sources.snippets",
+                opts = {
+                    friendly_snippets = false,
+                    search_paths = { vim.fn.stdpath("config") .. "/snippets" },
+                    global_snippets = { "all" },
+                    extended_filetypes = {},
+                    ignored_filetypes = {},
+                    get_filetype = function(context)
+                        return vim.bo.filetype
+                    end,
+                },
+            },
+            dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+            codeium = {
+                name = "codeium",
+                module = "blink.compat.source",
+                score_offset = -3,
+                max_items = 4,
+            },
+            lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+        },
+    },
+
+    appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+    },
+}
+
+M.config = function(_, opts)
+    require("blink.cmp").setup(opts)
+end
 
 return M
