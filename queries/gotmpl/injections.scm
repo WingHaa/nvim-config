2f03f67c14e72f67531161d12e3ae584501731ed
WingHaa <quanlm24@gmail.com>
2024-11-10 23:15:30 +0700

    remove injections.scm for go cause too buggy

--- queries/gotmpl/injections.scm
+++
@@ -1,7 +0,0 @@
-((text) @injection.content
- (#set! injection.language "html")
- (#set! injection.combined))
-
-((text) @injection.content
- (#set! injection.language "javascript")
- (#set! injection.combined))
