b556119b0bbdfa3c3d9898eed94d4c7d47829731
WingHaa <quanlm24@gmail.com>
2024-11-25 20:46:56 +0700

    add flutter

---
+++ lua/plugins/lsp/flutter-tools.lua
@@ -0,0 +1,29 @@
+return {
+    "nvim-flutter/flutter-tools.nvim",
+    lazy = false,
+    config = function()
+        vim.keymap.set("n", "<leader>lfs", ":FlutterRun <CR>", {})
+        vim.keymap.set("n", "<leader>lfq", ":FlutterQuit <CR>", {})
+        vim.keymap.set("n", "<leader>lfr", ":FlutterRestart <CR>", {})
+        vim.keymap.set("n", "<leader>lr", ":FlutterLspRestart <CR>", {})
+        vim.keymap.set("n", "<leader>lfd", ":FlutterDevTools <CR>", {})
+
+        require("flutter-tools").setup({
+            decorations = {
+                statusline = {
+                    app_version = true,
+                    device = true,
+                },
+            },
+            dev_tools = {
+                autostart = true,
+                auto_open_browser = true,
+            },
+            lsp = {
+                color = {
+                    enabled = true,
+                },
+            },
+        })
+    end,
+}
