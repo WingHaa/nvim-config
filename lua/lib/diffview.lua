c46aa6b43fcced7221bd6bddd2aaeab91a65e982
WingHaa <quanlm24@gmail.com>
2025-04-18 21:39:25 +0700

    update

--- lua/lib/diffview.lua
+++ lua/lib/diffview.lua
@@ -18,16 +18,18 @@ M.get_local_branches = function()
     if branch ~= "" then
         return M.parse_string(branch)
     else
-        return {}
+        return nil
     end
 end
 
 M.neo_review = function(opt)
-    vim.ui.select(M.get_local_branches(), { prompt = "Branch to compare with: " }, function(branch)
-        if opt.type == "branch" or not opt then
-            return vim.cmd("DiffviewOpen " .. branch .. "...HEAD --imply-local")
+    vim.ui.select(M.get_local_branches(), { prompt = "Branch to compare with: " }, function(choice)
+        if choice == nil then
+            return
+        elseif opt.type == "branch" or not opt then
+            return vim.cmd("DiffviewOpen " .. choice .. "...HEAD --imply-local")
         elseif opt.type == "commit" then
-            return vim.cmd("DiffviewFileHistory --range=" .. branch .. "...HEAD --right-only --no-merges")
+            return vim.cmd("DiffviewFileHistory --range=" .. choice .. "...HEAD --right-only --no-merges")
         end
     end)
 end
