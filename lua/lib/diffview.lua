7e45fb0d1c6dac16d334bf9203596444fb3537e4
WingHaa <quanlm24@gmail.com>
2025-05-14 09:06:51 +0700

    fix: diffview by commit

--- lua/lib/diffview.lua
+++ lua/lib/diffview.lua
@@ -13,7 +13,12 @@ local function parse_string(string)
     return branches
 end
 
-local function get_local_branches()
+local function get_recent_commits()
+    local output = vim.fn.systemlist("git log --pretty=format:'%h %s' -n 50")
+    return output
+end
+
+local function get_branches()
     local branch = vim.fn.system("git branch -a 2> /dev/null")
     if branch ~= "" then
         return parse_string(branch)
@@ -23,15 +28,22 @@ local function get_local_branches()
 end
 
 function M.neo_review(opt)
-    vim.ui.select(get_local_branches(), { prompt = "Branch to compare with: " }, function(choice)
-        if choice == nil then
-            return
-        elseif opt.type == "branch" or not opt then
-            return vim.cmd("DiffviewOpen " .. choice .. "...HEAD --imply-local")
-        elseif opt.type == "commit" then
-            return vim.cmd("DiffviewFileHistory --range=" .. choice .. "...HEAD --right-only --no-merges")
-        end
-    end)
+    if not opt or opt.type == "branch" then
+        vim.ui.select(get_branches(), { prompt = "Branch to compare with:" }, function(choice)
+            if choice then
+                vim.cmd("DiffviewOpen " .. choice .. "...HEAD --imply-local")
+            end
+        end)
+    elseif opt.type == "commit" then
+        vim.ui.select(get_recent_commits(), { prompt = "Commit to compare with:" }, function(choice)
+            if choice then
+                local sha = choice:match("^([0-9a-fA-F]+)")
+                if sha then
+                    vim.cmd("DiffviewFileHistory --range=" .. sha .. "...HEAD --right-only --no-merges")
+                end
+            end
+        end)
+    end
 end
 
 function M.set_diff_highlights()
