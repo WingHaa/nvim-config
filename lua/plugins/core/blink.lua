0b81bbe014dc690e3b26db047e83f5d20ddcca6a
WingHaa <quanlm24@gmail.com>
2025-05-23 14:46:18 +0700

    fix: snippets, gitsigns lazyloading

--- lua/plugins/core/blink.lua
+++ lua/plugins/core/blink.lua
@@ -22,44 +22,13 @@ M.dependencies = {
             },
         },
     },
-    {
-        "L3MON4D3/LuaSnip",
-        version = "v2.*",
-        build = "make install_jsregexp",
-        dependencies = {
-            "rafamadriz/friendly-snippets",
-            config = function()
-                require("luasnip.loaders.from_vscode").lazy_load()
-                require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
-
-                local extends = {
-                    typescript = { "tsdoc" },
-                    javascript = { "jsdoc" },
-                    lua = { "luadoc" },
-                    python = { "pydoc" },
-                    rust = { "rustdoc" },
-                    cs = { "csharpdoc" },
-                    java = { "javadoc" },
-                    c = { "cdoc" },
-                    cpp = { "cppdoc" },
-                    php = { "phpdoc" },
-                    kotlin = { "kdoc" },
-                    ruby = { "rdoc" },
-                    sh = { "shelldoc" },
-                }
-                -- friendly-snippets - enable standardized comments snippets
-                for ft, snips in pairs(extends) do
-                    require("luasnip").filetype_extend(ft, snips)
-                end
-            end,
-        },
-        opts = { history = true, delete_check_events = "TextChanged" },
-    },
 }
 
 M.opts = {
-    keymap = { preset = "enter" },
-    -- snippets = { preset = "luasnip" },
+    keymap = {
+        preset = "enter",
+        ["<C-y>"] = { "select_and_accept" }, -- make it so CR expand snippet with tabstop
+    },
     completion = {
         list = {
             max_items = 200,
@@ -135,9 +104,5 @@ M.opts = {
         nerd_font_variant = "mono",
     },
 }
-
-M.config = function(_, opts)
-    require("blink.cmp").setup(opts)
-end
 
 return M
