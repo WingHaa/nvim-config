9b0d1bc83658c8527e41bf9d71fc5b52c12e772e
WingHaa <quanlm24@gmail.com>
2026-04-21 10:42:12 +0700

    fix: leap
    remove flit

--- lua/plugins/editor/leap.lua
+++ lua/plugins/editor/leap.lua
@@ -1,12 +1,9 @@
 local L = { "https://codeberg.org/andyg/leap.nvim.git", keys = { "s", "S" } }
 
-local F = { "ggandor/flit.nvim" }
-
 L.opts = {
     max_phase_one_targets = nil,
     highlight_unlabeled_phase_one_targets = false,
     max_highlighted_traversal_targets = 10,
-    case_sensitive = false,
     equivalence_classes = { " \t\r\n" },
     substitute_chars = {},
     safe_labels = "fnut/SFNLHMUGTZ?",
@@ -29,17 +26,38 @@ L.config = function(_, opts)
     vim.keymap.set({ "n", "x", "o" }, "gs", function()
         require("leap.remote").action()
     end)
+
+    local function ft(key_specific_args)
+        require("leap").leap(vim.tbl_deep_extend("keep", key_specific_args, {
+            inputlen = 1,
+            inclusive = true,
+            opts = {
+                -- Force autojump.
+                labels = "",
+                -- Match the modes where you don't need labels (`:h mode()`).
+                safe_labels = vim.fn.mode(1):match("o") and "" or nil,
+            },
+        }))
+    end
+
+    -- A helper function making it easier to set "clever-f" behavior
+    -- (using f/F or t/T instead of ;/, - see the plugin clever-f.vim).
+    local clever = require("leap.user").with_traversal_keys
+    -- local clever_f = clever("f", "F")
+    local clever_t = clever("t", "T")
+
+    -- vim.keymap.set({ "n", "x", "o" }, "f", function()
+    --     ft({ opts = clever_f })
+    -- end)
+    -- vim.keymap.set({ "n", "x", "o" }, "F", function()
+    --     ft({ backward = true, opts = clever_f })
+    -- end)
+    vim.keymap.set({ "n", "x", "o" }, "t", function()
+        ft({ offset = -1, opts = clever_t })
+    end)
+    vim.keymap.set({ "n", "x", "o" }, "T", function()
+        ft({ backward = true, offset = 1, opts = clever_t })
+    end)
 end
 
-F.opts = {
-    keys = { f = "f", F = "F", t = "t", T = "T" },
-    -- A string like "nv", "nvo", "o", etc.
-    labeled_modes = "o",
-    clever_repeat = false,
-    multiline = false,
-    -- Like `leap`s similar argument (call-specific overrides).
-    -- E.g.: opts = { equivalence_classes = {} }
-    opts = {},
-}
-
-return { L, F }
+return { L }
