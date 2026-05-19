407f9be1c8ed4af18bb03e14445551b8cce53177
WingHaa <quanlm24@gmail.com>
2026-05-19 07:53:16 +0700

    fix: update oil config

--- lua/plugins/editor/oil.lua
+++ lua/plugins/editor/oil.lua
@@ -24,13 +24,13 @@ local M = {
 
 M.opts = {
     -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
-    -- Set to false if you still want to use netrw.
+    -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
     default_file_explorer = true,
     -- Id is automatically added at the beginning, and name at the end
     -- See :help oil-columns
     columns = {
         "icon",
-        "permissions",
+        -- "permissions",
         -- "size",
         -- "mtime",
     },
@@ -62,6 +62,8 @@ M.opts = {
     -- Note that the cleanup process only starts when none of the oil buffers are currently displayed
     cleanup_delay_ms = 2000,
     lsp_file_methods = {
+        -- Enable or disable LSP file operations
+        enabled = true,
         -- Time to wait for LSP file operations to complete before skipping
         timeout_ms = 1000,
         -- Set to true to autosave buffers that are updated with LSP willRenameFiles
@@ -72,7 +74,7 @@ M.opts = {
     -- Set to `false` to disable, or "name" to keep it on the file names
     constrain_cursor = "name",
     -- Set to true to watch the filesystem for changes and reload oil
-    experimental_watch_for_changes = false,
+    watch_for_changes = false,
     -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
     -- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
     -- Additionally, if it is a string that matches "actions.<name>",
@@ -80,27 +82,23 @@ M.opts = {
     -- Set to `false` to remove a keymap
     -- See :help oil-actions for a list of all available actions
     keymaps = {
-        ["g?"] = "actions.show_help",
+        ["g?"] = { "actions.show_help", mode = "n" },
         ["<CR>"] = "actions.select",
-        ["<C-v>"] = "actions.select_vsplit",
-        ["<C-s>"] = "actions.select_split",
-        ["<C-t>"] = "actions.select_tab",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
         ["<C-p>"] = "actions.preview",
-        ["<ESC>"] = "actions.close",
-        ["<leader>e"] = "actions.close",
-        ["<C-r>"] = "actions.refresh",
-        ["-"] = "actions.parent",
-        ["_"] = "actions.open_cwd",
-        ["`"] = "actions.cd",
-        ["~"] = "actions.tcd",
-        ["gs"] = "actions.change_sort",
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        ["<Esc>"] = { "actions.close", mode = "n" },
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["g~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
         ["gx"] = "actions.open_external",
-        ["g."] = "actions.toggle_hidden",
-        ["g\\"] = "actions.toggle_trash",
-    },
-    -- Configuration for the floating keymaps help window
-    keymaps_help = {
-        border = "rounded",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
     },
     -- Set to false to disable all of the above keymaps
     use_default_keymaps = true,
@@ -109,38 +107,80 @@ M.opts = {
         show_hidden = false,
         -- This function defines what is considered a "hidden" file
         is_hidden_file = function(name, bufnr)
-            return vim.startswith(name, ".") and not vim.startswith(name, ".config")
+            local m = name:match("^%.")
+            return m ~= nil
         end,
         -- This function defines what will never be shown, even when `show_hidden` is set
         is_always_hidden = function(name, bufnr)
             return false
         end,
-        -- Sort file names in a more intuitive order for humans. Is less performant,
-        -- so you may want to set to false if you work with large directories.
-        natural_order = true,
+        -- Sort file names with numbers in a more intuitive order for humans.
+        -- Can be "fast", true, or false. "fast" will turn it off for large directories.
+        natural_order = "fast",
+        -- Sort file and directory names case insensitive
+        case_insensitive = false,
         sort = {
             -- sort order can be "asc" or "desc"
             -- see :help oil-columns to see which columns are sortable
             { "type", "asc" },
             { "name", "asc" },
         },
+        -- Customize the highlight group for the file name
+        highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
+            return nil
+        end,
+    },
+    -- Extra arguments to pass to SCP when moving/copying files over SSH
+    extra_scp_args = {},
+    -- Extra arguments to pass to aws s3 when creating/deleting/moving/copying files using aws s3
+    extra_s3_args = {},
+    -- EXPERIMENTAL support for performing file operations with git
+    git = {
+        -- Return true to automatically git add/mv/rm files
+        add = function(path)
+            return false
+        end,
+        mv = function(src_path, dest_path)
+            return false
+        end,
+        rm = function(path)
+            return false
+        end,
     },
     -- Configuration for the floating window in oil.open_float
     float = {
         -- Padding around the floating window
         padding = 2,
+        -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
         max_width = 0,
         max_height = 0,
         border = "rounded",
         win_options = {
             winblend = 0,
         },
+        -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+        get_win_title = nil,
+        -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+        preview_split = "auto",
         -- This is the config that will be passed to nvim_open_win.
         -- Change values here to customize the layout
         override = set_float_size,
     },
-    -- Configuration for the actions floating preview window
-    preview = {
+    -- Configuration for the file preview window
+    preview_win = {
+        -- Whether the preview window is automatically updated when the cursor is moved
+        update_on_cursor_moved = true,
+        -- How to open the preview window "load"|"scratch"|"fast_scratch"
+        preview_method = "fast_scratch",
+        -- A function that returns true to disable preview on a file e.g. to avoid lag
+        disable_preview = function(filename)
+            return false
+        end,
+        -- Window-local options to use for preview window buffers
+        win_options = {},
+    },
+    -- Configuration for the floating action confirmation window
+    confirmation = {
         -- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
         -- min_width and max_width can be a single value or a list of mixed integer/float types.
         -- max_width = {100, 0.8} means "the lesser of 100 columns or 80% of total"
@@ -161,8 +201,6 @@ M.opts = {
         win_options = {
             winblend = 0,
         },
-        -- Whether the preview window is automatically updated when the cursor is moved
-        update_on_cursor_moved = true,
     },
     -- Configuration for the floating progress window
     progress = {
@@ -180,6 +218,10 @@ M.opts = {
     },
     -- Configuration for the floating SSH window
     ssh = {
+        border = "rounded",
+    },
+    -- Configuration for the floating keymaps help window
+    keymaps_help = {
         border = "rounded",
     },
 }
