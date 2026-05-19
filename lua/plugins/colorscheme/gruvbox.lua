d3f87f22314c4fa6a23b27a6a35ce91d82df2b6c
WingHaa <quanlm24@gmail.com>
2026-05-19 07:53:31 +0700

    fix: update gruvbox

--- lua/plugins/colorscheme/gruvbox.lua
+++ lua/plugins/colorscheme/gruvbox.lua
@@ -1,7 +1,29 @@
 return {
-    "morhetz/gruvbox",
+    "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = function()
-        vim.g.gruvbox_contrast_dark = "medium"
+        require("gruvbox").setup({
+            terminal_colors = true, -- add neovim terminal colors
+            undercurl = true,
+            underline = true,
+            bold = true,
+            italic = {
+                strings = true,
+                emphasis = true,
+                comments = true,
+                operators = false,
+                folds = true,
+            },
+            strikethrough = true,
+            invert_selection = false,
+            invert_signs = false,
+            invert_tabline = false,
+            inverse = true, -- invert background for search, diffs, statuslines and errors
+            contrast = "", -- can be "hard", "soft" or empty string
+            palette_overrides = {},
+            overrides = {},
+            dim_inactive = false,
+            transparent_mode = false,
+        })
     end,
 }
